c5aa2b5b44c37c80d0654a468a2082280a1b026d
yellhtut <yellhtutpromax@gmail.com>
2025-10-28 12:22:49 +0400

    folder structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,17 @@
 import { Stack } from "expo-router";
+import {SafeAreaView} from "react-native-safe-area-context";
 import "./global.css";
 
 export default function RootLayout() {
   return (
+  <SafeAreaView>
     <Stack>
       <Stack.Screen
-        name="(tabs)"
-        options={{headerShown: false}}
+          name="(tabs)"
+          options={{headerShown: false}}
       />
     </Stack>
+  </SafeAreaView>
+
   );
 }
